38d52d9dea0ff1396533e0cfd1139170163ab7ce
Kyueun Choi <kyueun.choi.q@gamil.com>
2019-01-12 17:21:03 +0900

    feat: using if&for

---
+++ algorithm/src/com/java/fortest/AsterikForTest.java
@@ -0,0 +1,27 @@
+package com.java.fortest;
+
+import java.util.Scanner;
+
+public class AsterikForTest {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+	
+	}
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
---
+++ algorithm/src/com/java/fortest/SumTest.java
@@ -0,0 +1,10 @@
+package com.java.fortest;
+
+import java.util.Scanner;
+
+public class SumTest {
+
+	public static void main(String[] args) {
+		
+	}
+}	
