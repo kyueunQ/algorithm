85c8aff912ebc1027388ee6189794fd39ab49d07
Kyueun Choi <kyueun.choi.q@gamil.com>
2019-01-19 00:31:08 +0900

    feat: add another solution of Problem04

--- algorithm/src/com/java/arraytest/Problem04.java
+++ algorithm/src/com/java/arraytest/Problem04.java
@@ -6,16 +6,15 @@ public class Problem04 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("금액: "); int money = sc.nextInt();
-		int[] arrMoney = new int[10];
+		System.out.print("금액: "); int money = sc.nextInt();
+		int separateMoney[] = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1};
 		int value = 0;
 		
-		for (int i=50000; i>0; i=i/5) {
+		for (int i=0; i<separateMoney.length; i++) {
 			
-			
-			value = money/i;
-			money -= value*i;
-			System.out.println(i + "원 : " + value + "개");
+			value = money/separateMoney[i];
+			money -= value*separateMoney[i];
+			System.out.println(separateMoney[i] + "원:" + value + "개");
 		}
 		
 		sc.close();
