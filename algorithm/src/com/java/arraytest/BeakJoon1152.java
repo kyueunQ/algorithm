1ee7686dd5c4446f670c9bfd9aa152ac8afaf42d
Kyueun Choi <kyueun.choi.q@gamil.com>
2019-01-21 23:41:42 +0900

    feat: add BeakJoon 1152

---
+++ algorithm/src/com/java/arraytest/BeakJoon1152.java
@@ -0,0 +1,19 @@
+package com.java.arraytest;
+
+import java.util.Scanner;
+
+public class BeakJoon1152 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String clause = sc.nextLine().trim();
+		
+		String word[] = clause.split(" ");
+		
+		
+		System.out.println((clause.isEmpty())? 0 : word.length);
+		
+	}
+
+}
---
+++ algorithm/src/com/java/arraytest/Problem05.java
@@ -0,0 +1,11 @@
+package com.java.arraytest;
+
+import java.util.Scanner;
+
+public class Problem05 {
+	
+	Scanner sc = new Scanner(System.in);
+	int in = sc.nextInt();
+
+
+}
