4e435da4b6770b07d969757ce567d8b32ae7954a
Kyueun Choi <kyueun.choi.q@gamil.com>
2019-01-22 23:53:03 +0900

    feat: add

---
+++ algorithm/src/com/java/fortest/BeakJoon1924.java
@@ -0,0 +1,14 @@
+package com.java.fortest;
+
+import java.util.Scanner;
+
+public class BeakJoon1924 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+	}
+
+}
--- algorithm/src/com/java/fortest/TESTEST.java
+++
@@ -1,5 +0,0 @@
-package com.java.fortest;
-
-public class TESTEST {
-
-}
